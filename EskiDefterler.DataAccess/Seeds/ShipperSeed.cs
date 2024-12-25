0a7eb1238a857426dc8b1df9dd32d69f68304b9c
ozdemirsevval <sevvalozdemir2002@windowslive.com>
2024-12-25 16:13:14 +0300

    Product ve Shipper Seed Data Yazıldı.

---
+++ EskiDefterler.DataAccess/Seeds/ShipperSeed.cs
@@ -0,0 +1,40 @@
+﻿using EskiDefterler.Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EskiDefterler.DataAccess.Seeds
+{
+    public class ShipperSeed : IEntityTypeConfiguration<Shipper>
+    {
+        public void Configure(EntityTypeBuilder<Shipper> builder)
+        {
+            builder.HasData(
+
+                new Shipper
+                {
+                    Id = 1,
+                    CompanyName = "JetKargo",
+                    Phone = "+90 555 444 55 55"
+                },
+                new Shipper
+                {
+                    Id = 2,
+                    CompanyName = "Aras Kargo",
+                    Phone = "444 25 52"
+                },
+                new Shipper
+                {
+                    Id = 3,
+                    CompanyName = "PTT Kargo",
+                    Phone = "+90 312 309 51 44"
+                }
+            );
+        }
+    }
+}
